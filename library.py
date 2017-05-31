b0405bbe067ed1021e5433b5a14dd2797275088e
Wojciech Koszela <Wojasinho@wojtek-server.localdomain>
2017-05-31 23:58:54 +0200

    Konstrukcja klasy + imlementacja introduce

--- library.py
+++ library.py
@@ -13,15 +13,46 @@ e-mail: 219263@student.pwr.edu.pl
 
 import cx_Oracle
 
-# Defined connection
-con = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
+class Library() :
 
-cur = con.cursor()
+    def __init__(self):
+        self.con = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
 
-# a = raw_input("Co chcesz zrobić")
-cur.execute('select * from dbadmin.books')
+    def interface(self):
 
-res = cur.fetchmany(numRows=3)
-print res[1]
+        intro = (
+            "Witamy w Bibliotece " + "Libary_SQL . Zdefiniuj co chcesz zrobić ? \n")
 
-cur.close()
+        self.lista_funkcji = [
+            "\n1. : Sprawdź dostępność interesującej książki",
+            "2. : Dodaj nową książke",
+            "3. : Usuń książkę"]
+
+        print intro
+
+        for gwiazdka in range(30):
+            print "*",
+
+        for opcja in self.lista_funkcji :
+            print opcja
+
+        wybor = raw_input(" \nWybierz z listy funkcję  ")
+
+        #Konieczna konwersja przy wywołaniu zawartości z tablicy na int
+        print ("Wybrano opcję :    " +  self.lista_funkcji[int(wybor)-1])
+
+
+# Stworzenie objektu klasy Library
+aplikacja = Library()
+aplikacja.interface()
+
+
+
+#     res = cur.fetchmany(numRows=3)
+#     print res[1]
+#
+#
+# cur.close()
+# cur = con.cursor()
+# # a = raw_input("Co chcesz zrobić")
+# cur.execute('select * from dbadmin.books')
