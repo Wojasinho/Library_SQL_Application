5838d8b5de83746fce8436821acae6c6e0abdd0a
Wojciech Koszela <Wojasinho@wojtek-server.localdomain>
2017-06-04 02:23:36 +0200

    Ukonczona metoda sprawdz_ksiazke(), zlicz_egzemplarze() Dziala aplikacja z tymi dwiema metodami, reszte do poprawy

--- library.py
+++ library.py
@@ -15,145 +15,206 @@ import cx_Oracle
 import os
 
 
+class App():
+    def __init__(self):
 
-# Inicjalizacyjne połączenie się do bazy na użytkownika dbadmin // Mało 'compliance' - hasło dostępne
-# Można pomyśleć, w jaki sposób to obejść / można np. ograniczył użytkownikowi DBADMIN uprawnienia (jedna z metod)
+        # Inicjalizacyjne połączenie się do bazy na użytkownika dbadmin // Mało 'compliance' - hasło dostępne
+        # Można pomyśleć, w jaki sposób to obejść / można np. ograniczył użytkownikowi DBADMIN uprawnienia (jedna z metod)
+        self.polaczenie = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
 
-polaczenie = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
+    def interface(self):
+        # Ustawienia triggera do funkcji 2
+        self.trigger = 0
+        intro = (
+            "Witamy w Bibliotece " + "Libary_SQL . Zdefiniuj co chcesz zrobić ? \n")
 
-def interface():
+        # Definicja listy funkcji
+        lista_funkcji = [
+            "1. : Sprawdź dostępność interesującej książki",
+            "2. : Sprawdź ilość egzemplarzy książki",
+            "3. : Dodaj nową książkę",
+            "4. : Usuń książkę"]
 
-    intro = (
-        "Witamy w Bibliotece " + "Libary_SQL . Zdefiniuj co chcesz zrobić ? \n")
+        for gwiazdka in range(30):
+            print "*",
+        print ('\n')
+        print (intro)
 
-    # Definicja listy funkcji
-    lista_funkcji = [
-        "1. : Sprawdź dostępność interesującej książki",
-        "2. : Sprawdź ilość egzemplarzy książki",
-        "3. : Dodaj nową książkę",
-        "4. : Usuń książkę"]
+        # Gwiazdki mające na celu poprawienie wyglądu aplikacji
+        for gwiazdka in range(30):
+            print "*",
 
-    print (intro)
+        print ('\n')
 
-    # Gwiazdki mające na celu poprawienie wyglądu aplikacji
-    for gwiazdka in range(30):
-        print "*",
+        for opcja in lista_funkcji:
+            print (opcja)
 
-    print ('\n')
+        pytanie_1 = raw_input(" \nWybierz z listy funkcję  ")
+        # Dodatkowo liczymy tablice od zera stąd wymagana odjecie -1
 
-    for opcja in lista_funkcji:
-        print (opcja)
+        pytanie_1 = int(pytanie_1)
+        #
+        # Konieczna konwersja "pytanie_1" przy wywołaniu w następnym etapi zawartości z tablicy na int
+        print ("Wybrano opcję :    " + lista_funkcji[int(pytanie_1) - 1])
 
-    pytanie_1 = raw_input(" \nWybierz z listy funkcję  ")
-    # Dodatkowo liczymy tablice od zera stąd wymagana odjecie -1
+        # Wywoływanie funkcji zależnie od decyzji podjętej przez użytkownika aplikacji
+        if (pytanie_1 == 1):
+            self.sprawdz_ksiazke()
 
-    pytanie_1 = int(pytanie_1)
-    #
-    # Konieczna konwersja "pytanie_1" przy wywołaniu w następnym etapi zawartości z tablicy na int
-    print ("Wybrano opcję :    " + lista_funkcji[int(pytanie_1) - 1])
+        if (pytanie_1 == 2):
+            self.zlicz_egzemplarze()
 
-    # Wywoływanie funkcji zależnie od decyzji podjętej przez użytkownika aplikacji
-    if (pytanie_1 == 1):
-        sprawdz_ksiazke()
+        if (pytanie_1 == 3):
+            # dodaj_ksiazke()
+            pass
 
-    if (pytanie_1 == 2):
-        zlicz_egzemplarze()
+        if (pytanie_1 == 4):
+            self.usun_ksiazke()
+            pass
 
+    def sprawdz_ksiazke(self):
 
-    if (pytanie_1 == 3):
-        # usun_ksiazke()
-        pass
+        while True:
+            self.podaj_ISBN = raw_input("Podaj proszę poprawny ISBN Książki (np.9788311130487)  : \n")
+            dlugosc = len(self.podaj_ISBN)
 
-def sprawdz_ksiazke():
+            # Warunek który jest potrzebny przy sprawdzeniu formatu ISBN, musimy uniknąc sytacji kiedy użytkownik wpisze
+            # inny format ISBN ( ISBN składa się z 13 cyfr ; do 31 grudnia 2006 r. zawierał 10 cyfr)
+            if (self.podaj_ISBN.isdigit() and (dlugosc == 13 or 10)):
+                print ("Wprowadzono poprawny ISBN książki")
+                # W tym miejscu wychodzimy z pętli while i kontynujemy aplikację
+                break
+            else:
+                print ("\nWprowadzono zły format ISBN książki")
 
-    while True:
-        podaj_ISBN = raw_input("Podaj proszę poprawny ISBN Książki (np.9788311130487)  : \n")
-        dlugosc=len(podaj_ISBN)
+                # W tym miejscu wracamy na początek pętli While
+                continue
 
+        # Gwiazdki mające na celu poprawienie wyglądu aplikacji
+        for gwiazdka in range(30):
+            print "*",
 
-        # Warunek który jest potrzebny przy sprawdzeniu formatu ISBN, musimy uniknąc sytacji kiedy użytkownik wpisze
-        # inny format ISBN ( ISBN składa się z 13 cyfr ; do 31 grudnia 2006 r. zawierał 10 cyfr)
-        if (podaj_ISBN.isdigit() and (dlugosc == (13 or 10))):
-            print ("Wprowadzono poprawny ISBN książki")
-            # W tym miejscu wychodzimy z pętli while i kontynujemy aplikację
-            break
+        # Nowa linia w celu poprawienia wyglądu aplikacji
+        print ("\n")
+
+        cur = self.polaczenie.cursor()
+
+        # Konstrukcja zapytania
+        zapytanie_ISBN = "select TITLE from DBADMIN.BOOKS where id_books =" + self.podaj_ISBN
+
+        # Wywołanie w CMDL SQL zapytania
+        cur.execute(zapytanie_ISBN)
+
+        odpowiedz_bazy = cur.fetchmany(numRows=3)
+
+        try:
+            # Wyciecie tytułu z zapytania
+            self.tytul_znaleziony = str(odpowiedz_bazy[0][0])
+        except:
+            IndexError
+            pass
+
+        if len(odpowiedz_bazy) == 0:
+            print "Nie znaleziono książki o " + self.podaj_ISBN + " w bazie danych"
+
+            # Efekt czystego ekranu i uruchom interfejs bazy
+            # print("\n\n\n")
+            return False
+            self.zakoncz()
+
         else:
-            print ("\nWprowadzono zły format ISBN książki")
+            print ("Znaleziono książkę o ISBN : " + self.podaj_ISBN + " w bazie danych.  "
+                                                                 "Tytuł szukanej książki to : '" + self.tytul_znaleziony) + "'"
+            return True
 
-            # W tym miejscu wracamy na początek pętli While
-            continue
+        # print odpowiedz_bazy
 
-    # Gwiazdki mające na celu poprawienie wyglądu aplikacji
-    for gwiazdka in range(30):
-        print "*",
+        pytanie__2 = raw_input((
+                               " \nCzy chcesz sprawdzić liczbę dostępnych egzemplarzy książki : '" + self.tytul_znaleziony + "' ? [tak / nie]\n"))
 
-    # Nowa linia w celu poprawienia wyglądu aplikacji
-    print ("\n")
+        if pytanie__2 == "tak":
+            # Zmienna trigger majaca na celu pominiecie w nastepnej metodzie zapytania o podanie nazwy szukanego tytułu
+            self.trigger = 1
+            self.zlicz_egzemplarze()
 
-    cur = polaczenie.cursor()
+        if pytanie__2 == "nie":
+            self.interface()
+            # Efekt czystego ekranu i uruchom interfejs bazy
 
-    # Konstrukcja zapytania
-    zapytanie_ISBN = "select TITLE from DBADMIN.BOOKS where id_books =" + podaj_ISBN
+            # print("\n\n\n\n\n\n\n")
+            # aplikacja.interface()
 
-    # Wywołanie w CMDL SQL zapytania
-    cur.execute(zapytanie_ISBN)
+    def zlicz_egzemplarze(self):
+        # polaczenie = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
+        cur = self.polaczenie.cursor()
 
-    odpowiedz_bazy = cur.fetchmany(numRows=3)
-
-    try:
-        # Wyciecie tytułu
-        tytul_znaleziony=str(odpowiedz_bazy[0][0])
-    except:
-        IndexError
-        pass
-
-    if len(odpowiedz_bazy) == 0 :
-        print "Nie znaleziono książki o " + podaj_ISBN + " w bazie danych"
-
-        # Efekt czystego ekranu i uruchom interfejs bazy
-        # print("\n\n\n")
-        interface()
-
-    else :
-        print ("Znaleziono książkę o ISBN : " + podaj_ISBN + " w bazie danych.  "
-                                                             "Tytuł szukanej książki to : "+tytul_znaleziony)
-
-    # print odpowiedz_bazy
-
-    pytanie__2 = raw_input((" \nCzy chcesz sprawdzić liczbę dostępnych egzemplarzy książki "+ tytul_znaleziony + " ? [tak / nie]\n")                               )
-
-    if pytanie__2 == "tak" :
-        zlicz_egzemplarze()
+        if self.trigger == 0:
+            self.podaj_tytul = raw_input("Podaj proszę tytuł interesującej Książki (np. Steve Jobs)  : \n")
 
 
-    if pytanie__2== "nie" :
-        interface()
-        # Efekt czystego ekranu i uruchom interfejs bazy
+        else:
+            self.podaj_tytul = self.tytul_znaleziony
+        # Skonstruowanie zapytania
+        zapytanie_egzemplarz = ("SELECT COUNT(1) FROM DBADMIN.BOOKS B, DBADMIN.COPY C  WHERE B.ID_BOOKS = C.ID_BOOKS  " \
+                                "AND TITLE ='%s'" % self.podaj_tytul)
 
-        # print("\n\n\n\n\n\n\n")
-        # aplikacja.interface()
+        # Wywołanie w CMDL SQL zapytania
+        cur.execute(zapytanie_egzemplarz)
 
-def zlicz_egzemplarze():
-    polaczenie = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
-    cur = polaczenie.cursor()
-    podaj_tytul = raw_input("Podaj proszę tytuł interesującej Książki (np. Steve Jobs)  : \n")
+        odpowiedz_bazy = cur.fetchmany(numRows=3)
+        print ("Znaleziono w bazie danych " + str(
+            odpowiedz_bazy[0][0]) + " egzemplarze książki :" + "'" + self.podaj_tytul + "'")
 
-    # Skonstruowanie zapytania
-    zapytanie_egzemplarz = "SELECT COUNT(1) FROM DBADMIN.BOOKS B, DBADMIN.COPY C  WHERE B.ID_BOOKS = C.ID_BOOKS  " \
-                           "AND TITLE ='%s'" % (podaj_tytul)
+        self.zakoncz()
 
+    def dodaj_ksiazke(self):
+        self.pytanie_4 = raw_input("")
 
+        slownik = {"ISBN": int, "Tytul": None, "Imie autora": None, "Nazwisko autora": None, "Wydawnictwo": None,
+                   "Miejsce wydania": None, "Ilosc stron": None, "Indeks egzemplarza": None}
 
-    # Wywołanie w CMDL SQL zapytania
-    cur.execute(zapytanie_egzemplarz)
+        kolejnosc = ["ISBN", "Tytul", "Imie autora", "Nazwisko autora", "Wydawnictwo", "Miejsce wydania", "Ilosc stron",
+                     "Indeks egzemplarza"]
 
-    odpowiedz_bazy = cur.fetchmany(numRows=3)
-    print ("Znaleziono w bazie danych " + str(odpowiedz_bazy[0][0])+ " egzemplarze książki " + podaj_tytul)
+        self.polaczenie = cx_Oracle.connect('dbadmin/password@192.168.1.103/library')
+
+        for i, j in enumerate(kolejnosc):
+            # j = raw_input("Podaj dla %r " %(j))
+            slownik[j] = raw_input("Podaj dla %r " % (j))
+
+        # zapytanie = "INSERT INTO DBADMIN.BOOKS(ID_BOOKS, TITLE, NAME, LASTNAME, PUBLISHING_HOUS, YEAR, PLACE_OF_PUBLISH, " \
+        #             "NUMBER_OF_PUBLIC, PAGE,ID_CATHEGORY) VALUES("+slownik[kolejnosc[0]]","+
+        #             slownik[kolejnosc[0]]"," slownik[kolejnosc[2]], slownik[kolejnosc[3]],
+        #        slownik[kolejnosc[4]], slownik[kolejnosc[5]], slownik[kolejnosc[6]], slownik[kolejnosc[7]],
+        #        slownik[kolejnosc[8]]);
+        print slownik
 
 
 
+    def usun_ksiazke(self):
+
+        if self.sprawdz_ksiazke() == False :
+
+        if self.sprawdz_ksiazke() == True :
+            pytanie_usun = raw_input("Czy na pewno chcesz usunąć książkę o ISBN : "+self.podaj_ISBN +"? [tak]/[nie]""\n")
+
+            if pytanie_usun == "tak" :
+                # Zapytanie o dropowanie wiersza
+            else :
+                self.zakoncz()
+
+    def zakoncz(self):
+
+        self.pytanie_3 = raw_input("Co robisz dalej : zakończ aplikacje / powrót  ?        [zakończ]/[powrót]""\n")
+        if self.pytanie_3 == "zakończ":
+            exit
+
+        if self.pytanie_3 == "powrót":
+            self.interface()
 
 
+aplikacja = App()
+aplikacja.interface()
 # interface()
-zlicz_egzemplarze()
+
 
